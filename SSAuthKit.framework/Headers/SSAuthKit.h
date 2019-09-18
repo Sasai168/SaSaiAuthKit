56e35c1ba7ddc64bfbe07b1be6f491b3c88404e2
SaSai168 <sasai@gigaiot.com>
2019-09-18 19:34:37 +0800

    修改kit

---
+++ SSAuthKit.framework/Headers/SSAuthKit.h
@@ -0,0 +1,23 @@
+//
+//  SSAuthKit.h
+//  SSAuthKit
+//
+//  Created by 谢雪滔 on 2019/9/18.
+//  Copyright © 2019 谢雪滔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GIGRetAuthCode.h"
+#import "GIGAppInfo.h"
+#import "GIGSasaiOAuth.h"
+
+
+//! Project version number for SSAuthKit.
+FOUNDATION_EXPORT double SSAuthKitVersionNumber;
+
+//! Project version string for SSAuthKit.
+FOUNDATION_EXPORT const unsigned char SSAuthKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSAuthKit/PublicHeader.h>
+
+
